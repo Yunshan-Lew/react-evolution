1c07cfdcbe53e9b65c7a8c32b6b4c6b53617eee5
luzhipeng <luzhipeng@luzhipeng>
2021-10-09 14:40:55 +0800

    HOC for auth

--- src/views/page/Page1.js
+++ src/views/page/Page1.js
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import actions from '@/store/actions';
 
 function Page1(props) {
   let { relation } = props.detailData
-
+  const history = useHistory()
   useEffect(() => {
     let { AjaxLogin } = props.actions
     document.title = 'Page1'
     AjaxLogin({
-      data: { username: "admin", password: "admin123456", type: "normal", tenantId: 1 }
+      data: { username: "admin", password: "admin123456", type: "normal", tenantId: 1 },
+      success: res => {
+        history.push({ pathname: '/home/page2' })
+      }
     })
-  }, [props.actions])
+  }, [props.actions, history])
 
   return <h1>This is Page1, { relation }</h1>
 }

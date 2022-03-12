e35053685237f4fdaea0eb94a644c32a240ad3da
卢玛西亚 <alexanderde1591@gmail.com>
2022-03-12 16:02:12 +0800

    Modify proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware('/api', {
-    target: 'http://172.16.10.151:8084/',
+    target: 'https://web.wwuche.com/api/',
     changeOrigin: true,
     pathRewrite: {
       '^/api': ''
